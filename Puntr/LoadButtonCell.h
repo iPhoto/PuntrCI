1e0ffce367cb5cdff59f913475f63b420276ec7c
Alexander Lebedev <alexander.lebedev@2nova.ru>
2013-07-24 15:31:18 +0400

    + LoadButtonCell ~ Catalogue(on one section) now displaying button instead of section cell

---
+++ Puntr/LoadButtonCell.h
@@ -0,0 +1,15 @@
+//
+//  LoadButtonCell.h
+//  Puntr
+//
+//  Created by Alexander Lebedev on 7/24/13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadButtonCell : UICollectionViewCell
+
+- (void)loadButton;
+
+@end
