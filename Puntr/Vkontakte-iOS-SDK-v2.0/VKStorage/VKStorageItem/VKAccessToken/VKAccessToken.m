2cacf66d0b7bc8c0be506e089f90f6747ced70b8
Alexander Lebedev <alexander.lebedev@2nova.ru>
2013-08-07 12:15:18 +0400

    + vk and tw libraries

---
+++ Puntr/Vkontakte-iOS-SDK-v2.0/VKStorage/VKStorageItem/VKAccessToken/VKAccessToken.m
@@ -0,0 +1,188 @@
+//
+// Created by AndrewShmig on 5/28/13.
+//
+//
+// Copyright (c) 2013 Andrew Shmig
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+#import "VKAccessToken.h"
+
+
+#define INFO_LOG() NSLog(@"%s", __FUNCTION__)
+
+
+@implementation VKAccessToken
+
+#pragma mark - Init methods
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    INFO_LOG();
+
+    self = [super init];
+
+    if(!self)
+        return nil;
+
+    self->_userID = (NSUInteger)[aDecoder decodeIntegerForKey:@"userID"];
+    self->_token = [aDecoder decodeObjectForKey:@"token"];
+    self->_expirationTime = [aDecoder decodeDoubleForKey:@"expirationTime"];
+    self->_permissions = [aDecoder decodeObjectForKey:@"permissions"];
+    self->_creationTime = [aDecoder decodeDoubleForKey:@"creationTime"];
+
+    return self;
+}
+
+- (instancetype)initWithUserID:(NSUInteger)userID
+                   accessToken:(NSString *)token
+                expirationTime:(NSTimeInterval)expirationTime
+                   permissions:(NSArray *)permissions
+{
+    INFO_LOG();
+
+    if (self = [super init]) {
+        _userID = userID;
+        _token = [token copy];
+        _expirationTime = expirationTime;
+        _permissions = [permissions copy];
+        _creationTime = [[NSDate date] timeIntervalSince1970];
+    }
+
+    return self;
+}
+
+- (instancetype)initWithUserID:(NSUInteger)userID
+                   accessToken:(NSString *)token
+                expirationTime:(NSTimeInterval)expirationTime
+{
+    INFO_LOG();
+
+    return [self initWithUserID:userID
+                    accessToken:token
+                 expirationTime:expirationTime
+                    permissions:@[]];
+}
+
+- (instancetype)initWithUserID:(NSUInteger)userID
+                   accessToken:(NSString *)token
+{
+    INFO_LOG();
+
+    return [self initWithUserID:userID
+                    accessToken:token
+                 expirationTime:0
+                    permissions:@[]];
+}
+
+- (instancetype)init
+{
+    INFO_LOG();
+
+    return [self initWithUserID:0
+                    accessToken:@""
+                 expirationTime:0
+                    permissions:@[]];
+}
+
+#pragma mark - Overriden methods
+
+- (NSString *)description
+{
+    INFO_LOG();
+
+    NSDictionary *desc = @{
+            @"User ID"         : @(self.userID),
+            @"Expiration time" : @(((NSUInteger) (self.creationTime + self.expirationTime))),
+            @"Creation time"   : @(((NSUInteger) self.creationTime)),
+            @"Permissions"     : self.permissions,
+            @"Token"           : self.token
+    };
+
+    return [desc description];
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    INFO_LOG();
+
+    [aCoder encodeInteger:_userID forKey:@"userID"];
+    [aCoder encodeObject:_token forKey:@"token"];
+    [aCoder encodeDouble:_expirationTime forKey:@"expirationTime"];
+    [aCoder encodeObject:_permissions forKey:@"permissions"];
+    [aCoder encodeDouble:_creationTime forKey:@"creationTime"];
+}
+
+- (BOOL)isEqual:(VKAccessToken *)token
+{
+    INFO_LOG();
+
+    NSSet *currentSet = [NSSet setWithArray:self.permissions];
+    NSSet *otherSet = [NSSet setWithArray:token.permissions];
+
+    return ([self.token isEqualToString:token.token] &&
+            [currentSet isEqualToSet:otherSet] &&
+            (self.userID == token.userID));
+}
+
+- (VKAccessToken *)copyWithZone:(NSZone *)zone
+{
+    INFO_LOG();
+
+    VKAccessToken *copyToken = [[VKAccessToken alloc] initWithUserID:self.userID
+                                                         accessToken:self.token
+                                                      expirationTime:self.expirationTime
+                                                         permissions:self.permissions];
+
+    return copyToken;
+}
+
+#pragma mark - Visible methods
+
+- (BOOL)hasPermission:(NSString *)permission
+{
+    INFO_LOG();
+
+    return [self.permissions containsObject:permission];
+}
+
+- (BOOL)isExpired
+{
+    INFO_LOG();
+
+    NSTimeInterval currentTimestamp = [[NSDate date] timeIntervalSince1970];
+
+    if (self.expirationTime == 0 && [self hasPermission:@"offline"])
+        return NO;
+    else
+        return ((self.expirationTime + self.creationTime) < currentTimestamp);
+}
+
+- (BOOL)isValid
+{
+    INFO_LOG();
+
+    return (nil != self.token && ![self isExpired]);
+}
+
+@end
