7853a600fba74da08fcc5733f0cdfef81c4bbcb5
Eugene Tulushev <eugene@tulushev.com>
2013-08-09 15:29:16 +0400

    + Empty LeadCell

---
+++ Puntr/LeadCell.h
@@ -0,0 +1,13 @@
+//
+//  LeadCell.h
+//  Puntr
+//
+//  Created by Eugene Tulushev on 09.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeadCell : UICollectionViewCell
+
+@end
---
+++ Puntr/LeadCell.m
@@ -0,0 +1,24 @@
+//
+//  LeadCell.m
+//  Puntr
+//
+//  Created by Eugene Tulushev on 09.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import "LeadCell.h"
+
+@implementation LeadCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+@end
