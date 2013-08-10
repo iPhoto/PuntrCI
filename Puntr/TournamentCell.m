8840699a9b1172f4d5389bff89f128f72b37c351
Momus <lastprimat@gmail.com>
2013-08-10 14:59:43 +0300

    +tournamentCell +tournamentViewController ~add tournament cell detection

---
+++ Puntr/TournamentCell.m
@@ -0,0 +1,31 @@
+//
+//  TournamentCell.m
+//  Puntr
+//
+//  Created by Momus on 10.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import "TournamentCell.h"
+
+@implementation TournamentCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Puntr/TournamentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TournamentViewController.h
+//  Puntr
+//
+//  Created by Momus on 10.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TournamentViewController : UIViewController
+
+@end
