9dbfa951de133ae614117282a903a5c186398484
Momus <lastprimat@gmail.com>
2013-08-11 21:46:35 +0300

    +utilities +tournament icon ~tournament controller ~tournament cell

---
+++ Puntr/PuntrUtilities.h
@@ -0,0 +1,13 @@
+//
+//  PuntrUtilities.h
+//  Puntr
+//
+//  Created by Momus on 11.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PuntrUtilities : NSObject
+
+@end
---
+++ Puntr/PuntrUtilities.m
@@ -0,0 +1,13 @@
+//
+//  PuntrUtilities.m
+//  Puntr
+//
+//  Created by Momus on 11.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import "PuntrUtilities.h"
+
+@implementation PuntrUtilities
+
+@end
