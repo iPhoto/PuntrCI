ea8023c65c33edead1c88edb683e60eae3daa017
artik818 <artik818@gmail.com>
2013-08-18 01:30:57 +0300

    settings screen with groupped tableView

--- Puntr/SettingsViewController.m
+++ Puntr/SettingsViewController.m
@@ -11,19 +11,41 @@
 #import <QuartzCore/QuartzCore.h>
 
 
-#define TABLE_CELLS_HEIGHT 30.0
+#define FONT_HEADER         [UIFont boldSystemFontOfSize:[UIFont systemFontSize]]
+#define FONT_FOOTER         [UIFont boldSystemFontOfSize:11.0f]
 
-#define FONT_CONF_LABEL     [UIFont fontWithName:@"Arial-BoldMT" size:11.0f]
+#define headerFooterSidePadding         14.0
+#define headerFooterTopPadding          8.0
+
+@interface SettingsTableViewCell : UITableViewCell
+
+@end
+
+
+@implementation SettingsTableViewCell
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.imageView.center = CGPointMake(20, self.imageView.center.y);
+    self.textLabel.frame = CGRectMake(40, self.textLabel.frame.origin.y, self.textLabel.frame.size.width, self.textLabel.frame.size.height);
+}
+
+@end
+
+// -----------------------------------------
 
 @interface SettingsViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, weak) UITableView *settingsTable1;
-@property (nonatomic, weak) UITableView *settingsTable2;
-@property (nonatomic, weak) UITableView *settingsTable3;
 
 @property (nonatomic, strong) NSArray *settingsArray;
+@property (nonatomic, strong) NSArray *sectionHeadersArray;
+@property (nonatomic, strong) NSArray *sectionFootersArray;
 
 @end
+
+
+
 
 @implementation SettingsViewController
 
@@ -31,117 +53,26 @@
 {
     [super viewDidLoad];
     
-    [self setupSettingsArray];
-    
-    CGRect fullScreen = [UIScreen mainScreen].bounds;
-    
-    self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
-    
-    UIScrollView *bkScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - 100)];
-    [self.view addSubview:bkScrollView];
-    
-    
-    CGFloat padding = 10;
-    CGFloat currentTop = 10;
-    
-    self.title = @"Настройки";
-    
     [self addBalanceButton];
     
-    UILabel *titleLabel;
+    [self setupSettingsArray];
     
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, currentTop, 280, 35)];
-    titleLabel.text = @"Профиль";
-    titleLabel.textColor = [UIColor whiteColor];
-    titleLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
-    titleLabel.backgroundColor = [UIColor clearColor];
-    [bkScrollView addSubview:titleLabel];
+    self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
+    self.title = @"Настройки";
     
-    currentTop += CGRectGetHeight(titleLabel.frame);
+    CGRect fr = self.view.bounds;
+    fr.size.height -= (CGRectGetHeight(self.tabBarController.tabBar.bounds) + CGRectGetHeight(self.navigationController.navigationBar.bounds));
     
-    NSInteger tableTag;
-    UITableView *tv;
-    UIImage *backgroundImage = [[UIImage imageNamed:@"settingsBackground"] resizableImageWithCapInsets:UIEdgeInsetsMake(4.0f, 4.0f, 4.0f, 17.0f)];
-    
-    tableTag = 0;
-    tv = [[UITableView alloc] initWithFrame:CGRectMake(padding, currentTop, CGRectGetWidth(fullScreen) - (padding * 2), TABLE_CELLS_HEIGHT * ((NSArray *)self.settingsArray[tableTag]).count - 2) style:UITableViewStylePlain];
+    UITableView *tv = [[UITableView alloc] initWithFrame:fr style:UITableViewStyleGrouped];
     self.settingsTable1 = tv;
     self.settingsTable1.dataSource = self;
     self.settingsTable1.delegate = self;
-    self.settingsTable1.backgroundView = [[UIImageView alloc] initWithImage:backgroundImage];
-    self.settingsTable1.layer.cornerRadius = 2.5;
+    self.settingsTable1.backgroundView = nil;
     [self.settingsTable1 setBackgroundColor:[UIColor clearColor]];
     self.settingsTable1.separatorColor = [UIColor colorWithWhite:175.0 / 255.0 alpha:1.0];
     self.settingsTable1.scrollEnabled = NO;
-    self.settingsTable1.tag = tableTag;
-    [bkScrollView addSubview:self.settingsTable1];
-    
-    currentTop += CGRectGetHeight(self.settingsTable1.frame);
-    currentTop += 10;
-    
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, currentTop, 280, 35)];
-    titleLabel.text = @"Аккаунт";
-    titleLabel.textColor = [UIColor whiteColor];
-    titleLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
-    titleLabel.backgroundColor = [UIColor clearColor];
-    [bkScrollView addSubview:titleLabel];
-    
-    currentTop += CGRectGetHeight(titleLabel.frame);
-    
-    tableTag++;
-    tv = [[UITableView alloc] initWithFrame:CGRectMake(padding, currentTop, CGRectGetWidth(fullScreen) - (padding * 2), TABLE_CELLS_HEIGHT * ((NSArray *)self.settingsArray[tableTag]).count - 2) style:UITableViewStylePlain];
-    self.settingsTable2 = tv;
-    self.settingsTable2.dataSource = self;
-    self.settingsTable2.delegate = self;
-    self.settingsTable2.backgroundView = [[UIImageView alloc] initWithImage:backgroundImage];
-    self.settingsTable2.layer.cornerRadius = 2.5;
-    [self.settingsTable2 setBackgroundColor:[UIColor clearColor]];
-    self.settingsTable2.separatorColor = [UIColor colorWithWhite:175.0 / 255.0 alpha:1.0];
-    self.settingsTable2.scrollEnabled = NO;
-    self.settingsTable2.tag = tableTag;
-    [bkScrollView addSubview:self.settingsTable2];
-    
-    currentTop += CGRectGetHeight(self.settingsTable2.frame);
-    currentTop += 10;
-    
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, currentTop, 280, 80)];
-    titleLabel.numberOfLines = 0;
-    titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
-    titleLabel.text = @"Установите настройку конфиденциальности в \"Да\", если хотите, чтоб ваши действия были невидимыми всем пользователям, кроме тех, на кого вы подписались. Если другой пользователь захочет подписаться на ваши действия, вы получите запрос";
-    titleLabel.textColor = [UIColor whiteColor];
-    titleLabel.font = FONT_CONF_LABEL;
-    titleLabel.backgroundColor = [UIColor clearColor];
-    [bkScrollView addSubview:titleLabel];
-    
-    currentTop += CGRectGetHeight(titleLabel.frame);
-    currentTop += 10;
-    
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, currentTop, 280, 35)];
-    titleLabel.text = @"Условия";
-    titleLabel.textColor = [UIColor whiteColor];
-    titleLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
-    titleLabel.backgroundColor = [UIColor clearColor];
-    [bkScrollView addSubview:titleLabel];
-    
-    currentTop += CGRectGetHeight(titleLabel.frame);
-    
-    tableTag++;
-    tv = [[UITableView alloc] initWithFrame:CGRectMake(padding, currentTop, CGRectGetWidth(fullScreen) - (padding * 2), TABLE_CELLS_HEIGHT * ((NSArray *)self.settingsArray[tableTag]).count - 2) style:UITableViewStylePlain];
-    self.settingsTable3 = tv;
-    self.settingsTable3.dataSource = self;
-    self.settingsTable3.delegate = self;
-    self.settingsTable3.backgroundView = [[UIImageView alloc] initWithImage:backgroundImage];
-    self.settingsTable3.layer.cornerRadius = 2.5;
-    [self.settingsTable3 setBackgroundColor:[UIColor clearColor]];
-    self.settingsTable3.separatorColor = [UIColor colorWithWhite:175.0 / 255.0 alpha:1.0];
-    self.settingsTable3.scrollEnabled = NO;
-    self.settingsTable3.tag = tableTag;
-    [bkScrollView addSubview:self.settingsTable3];
-
-    currentTop += CGRectGetHeight(self.settingsTable3.frame);
-    currentTop += 10;
-    
-    bkScrollView.contentSize = CGSizeMake(bkScrollView.frame.size.width, currentTop);
+    [self.settingsTable1 setScrollEnabled:YES];
+    [self.view addSubview:self.settingsTable1];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -155,31 +86,39 @@
     [super didReceiveMemoryWarning];
 }
 
-#pragma mark - UITableViewDataSource
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return TABLE_CELLS_HEIGHT;
-}
+
+#pragma mark - UITableViewDataSource
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    CGFloat pictureWidth = 150;
+    NSDictionary *cellDictionary = self.settingsArray[indexPath.section][indexPath.row];
     
-    NSDictionary *cellDictionary = self.settingsArray[tableView.tag][indexPath.item];
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SettingsViewCell"];
+    NSString *reuseIdFileName = @"middle.png";
+    if (indexPath.row == 0) {
+        reuseIdFileName = @"top.png";
+    }
+    if (indexPath.row == ((NSArray *)self.settingsArray[indexPath.section]).count - 1) {
+        reuseIdFileName = @"bottom.png";
+    }
+    
+    SettingsTableViewCell *cell = [[SettingsTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIdFileName];
+    cell.backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:reuseIdFileName]];
     cell.backgroundColor = [UIColor clearColor];
-    if ([cellDictionary[@"isAccessory"] boolValue])
+    
+    if (cellDictionary[@"title"]) {
+        cell.textLabel.text = cellDictionary[@"title"];
+        cell.textLabel.textColor = [UIColor whiteColor];
+        cell.textLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
+    }
+    
+    if ((cellDictionary[@"isAccessory"]) && ([cellDictionary[@"isAccessory"] boolValue]))
     {
 //        cell.accessoryView = 
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
-    cell.textLabel.text = cellDictionary[@"title"];
-    cell.textLabel.textColor = [UIColor whiteColor];
-    cell.textLabel.font = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
-    
-    if (![cellDictionary[@"pictureName"] isEqualToString:@""])
+    if ((cellDictionary[@"pictureName"]) && (![cellDictionary[@"pictureName"] isEqualToString:@""]))
     {
         cell.imageView.image = [UIImage imageNamed:cellDictionary[@"pictureName"]];
     }
@@ -187,16 +126,37 @@
     return cell;
 }
 
+
+// --- sections
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return self.settingsArray.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    NSInteger number = ((NSArray *)self.settingsArray[tableView.tag]).count;
+    NSInteger number = ((NSArray *)self.settingsArray[section]).count;
     return number;
 }
+
+// --- headers
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return [self heightForHeader:YES inSection:section];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    return [self viewForHeader:YES inSection:section];
+}
+
+// --- footers
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return [self heightForHeader:NO inSection:section];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return [self viewForHeader:NO inSection:section];
+}
+
 
 #pragma mark - UITableViewDelegate
 
@@ -216,16 +176,84 @@
                                 ],
                                @[
                                    @{ @"pictureName": @"4.png", @"title": @"Аккаунты соц. сетей", @"isAccessory": @(YES) },
-                                   @{ @"pictureName": @"5.png", @"title": @"Пригласить друзей из соцюсетей", @"isAccessory": @(YES) },
+                                   @{ @"pictureName": @"5.png", @"title": @"Пригласить друзей из соц. сетей", @"isAccessory": @(YES) },
                                    @{ @"pictureName": @"6.png", @"title": @"Push-уведомления", @"isAccessory": @(YES) },
-                                   @{ @"pictureName": @"7.png", @"title": @"Конфиденциальность", @"isAccessory": @(NO) },
-                                   @{ @"pictureName": @"8.png", @"title": @"Выйти", @"isAccessory": @(NO) },
+                                   @{ @"pictureName": @"7.png", @"title": @"Конфиденциальность"},
+                                   @{ @"pictureName": @"8.png", @"title": @"Выйти"},
+                                   @{ @"type": @"sectionFooter", @"title": @"Условия"},
                                 ],
                                @[
                                    @{ @"pictureName": @"9.png", @"title": @"Оферта", @"isAccessory": @(YES) },
-                                   @{ @"pictureName": @"10.png", @"title": @"Условия использования", @"isAccessory": @(YES) },
+                                   @{ @"pictureName": @"10.png", @"title": @"Условия использования"},
                                 ],
                          ];
+    
+    self.sectionHeadersArray = @[
+                                 @{ @"text": @"Профиль", @"font": FONT_HEADER},
+                                 @{ @"text": @"Аккаунт", @"font": FONT_HEADER},
+                                 @{ @"text": @"Условия", @"font": FONT_HEADER},
+                                 ];
+    
+    self.sectionFootersArray = @[
+                                 @{ @"text": @""},
+                                 @{ @"text": @"Установите настройку конфиденциальности в \"Да\", если хотите, чтоб ваши действия были невидимыми всем пользователям, кроме тех, на кого вы подписались. Если другой пользователь захочет подписаться на ваши действия, вы получите запрос", @"font": FONT_FOOTER},
+                                 @{ @"text": @""},
+                                 ];
+}
+
+- (CGFloat)heightForHeader:(BOOL)isHeader inSection:(NSInteger)section {
+    CGFloat retVal = UITableViewAutomaticDimension;
+    
+    NSDictionary *dic;
+    if (isHeader) {
+        dic = self.sectionHeadersArray[section];
+    }
+    else {
+        dic = self.sectionFootersArray[section];
+    }
+    
+    if (dic[@"text"] && (![dic[@"text"] isEqualToString:@""])) {
+        NSString *header = dic[@"text"];
+        CGSize headerSize = [header sizeWithFont:(UIFont *)dic[@"font"] constrainedToSize:CGSizeMake(self.settingsTable1.frame.size.width - (headerFooterSidePadding*2), MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping];
+        retVal = headerFooterTopPadding + headerSize.height + 5;
+    }
+    return retVal;
+}
+
+- (UIView *)viewForHeader:(BOOL)isHeader inSection:(NSInteger)section {
+    UIView *headerView = nil;
+    
+    NSDictionary *dic;
+    if (isHeader) {
+        dic = self.sectionHeadersArray[section];
+    }
+    else {
+        dic = self.sectionFootersArray[section];
+    }
+    
+    if (dic[@"text"] && (![dic[@"text"] isEqualToString:@""])) {
+        NSString *header = dic[@"text"];
+        CGSize headerSize = [header sizeWithFont:(UIFont *)dic[@"font"] constrainedToSize:CGSizeMake(self.settingsTable1.frame.size.width - (headerFooterSidePadding*2), MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping];
+        
+        headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.settingsTable1.frame.size.width, headerFooterTopPadding + headerSize.height + 5)];
+        //headerView.backgroundColor = [UIColor clearColor];
+        
+        UILabel *headerViewLabel = [[UILabel alloc] initWithFrame:CGRectMake(headerFooterSidePadding, headerFooterTopPadding, self.settingsTable1.frame.size.width - (headerFooterSidePadding*2), headerSize.height)];
+        headerViewLabel.text = header;
+        headerViewLabel.font = (UIFont *)dic[@"font"];
+        headerViewLabel.backgroundColor = [UIColor clearColor];
+        headerViewLabel.textColor = [UIColor whiteColor];
+        headerViewLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        headerViewLabel.numberOfLines = 0;
+        [headerView addSubview:headerViewLabel];
+    }
+    return headerView;
 }
 
 @end
+
+
+//    self.textFieldLogin.text = @"qqq@gmail.com";
+//    self.textFieldPassword.text = @"qqqqqq";
+
+
