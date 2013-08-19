a3afcfee9f45ce8b1a2359e9b9d0181645400f17
Eugene Tulushev <eugene@tulushev.com>
2013-08-19 13:09:56 +0400

    + NewsVC News Collection Integration

--- Puntr/NewsViewController.m
+++ Puntr/NewsViewController.m
@@ -6,9 +6,15 @@
 //  Copyright (c) 2013 2Nova Interactive. All rights reserved.
 //
 
+#import "CollectionManager.h"
 #import "NewsViewController.h"
 #import "UIViewController+Puntr.h"
+
+static const CGFloat TNItemSpacing = 12.0f;
+
 @interface NewsViewController ()
+
+@property (nonatomic, strong) CollectionManager *collectionManager;
 
 @end
 
@@ -21,12 +27,25 @@
     self.title = @"Новости";
     [self addBalanceButton];
     self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
+    
+    CGRect applicationFrame = [[UIScreen mainScreen] applicationFrame];
+    CGRect viewControllerFrame = CGRectMake(0.0f,
+                                            0.0f,
+                                            CGRectGetWidth(applicationFrame),
+                                            CGRectGetHeight(applicationFrame) - CGRectGetHeight(self.navigationController.navigationBar.bounds) - CGRectGetHeight(self.tabBarController.tabBar.bounds)
+                                            );
+    
+    self.collectionManager = [CollectionManager managerWithType:CollectionTypeNews modifierObject:nil];
+    UICollectionView *collectionView = self.collectionManager.collectionView;
+    collectionView.frame = viewControllerFrame;
+    [self.view addSubview:collectionView];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
     [self updateBalance];
+    [self.collectionManager reloadData];
 }
 
 @end
