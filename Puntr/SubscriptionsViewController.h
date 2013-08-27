f34def8ff365fbfbbb37976675cac3769d991f9d
Eugene Tulushev <eugene@tulushev.com>
2013-08-27 13:00:28 +0400

    + SubscriptionsVC

---
+++ Puntr/SubscriptionsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsViewController.h
+//  Puntr
+//
+//  Created by Eugene Tulushev on 27.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class UserModel;
+
+@interface SubscriptionsViewController : UIViewController
+
+- (id)initWithUser:(UserModel *)user;
+
+@end
---
+++ Puntr/SubscriptionsViewController.m
@@ -0,0 +1,53 @@
+//
+//  SubscriptionsViewController.m
+//  Puntr
+//
+//  Created by Eugene Tulushev on 27.08.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import "CollectionManager.h"
+#import "SubscriptionsViewController.h"
+#import "UIViewController+Puntr.h"
+#import "UserModel.h"
+
+@interface SubscriptionsViewController ()
+
+@property (nonatomic, strong, readonly) UserModel *user;
+
+@property (nonatomic, strong) CollectionManager *collectionManager;
+
+@end
+
+@implementation SubscriptionsViewController
+
+- (id)initWithUser:(UserModel *)user
+{
+    self = [super init];
+    if (self) {
+        _user = user;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	CGRect frame = self.frame;
+    
+    self.title = @"Подписки";
+    self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
+    
+    self.collectionManager = [CollectionManager managerWithType:CollectionTypeSubscriptions modifierObject:self.user];
+    UICollectionView *collectionView = self.collectionManager.collectionView;
+    collectionView.frame = frame;
+    [self.view addSubview:collectionView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.collectionManager reloadData];
+}
+
+@end
