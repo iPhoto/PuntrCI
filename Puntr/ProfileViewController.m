bc73e1dc1d768a0a9152a3587635c0d44c2046ae
Eugene Tulushev <eugene@tulushev.com>
2013-08-30 19:54:24 +0400

    - ProfileVC Junk

--- Puntr/ProfileViewController.m
+++ Puntr/ProfileViewController.m
@@ -290,8 +290,6 @@ static const CGFloat TNSideImageLarge = 60.0f;
 - (void)settingsButtonTouched
 {
     [self.navigationController pushViewController:[[SettingsViewController alloc] init] animated:YES];
-//    FilterViewController *fvc = [FilterViewController new];
-//    [self.navigationController pushViewController:fvc animated:YES];
 }
 
 - (void)loadProfile
