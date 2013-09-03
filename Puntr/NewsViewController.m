9704ea0aeb08985276b52886e04dc2bb28d3510c
Eugene Tulushev <eugene@tulushev.com>
2013-09-03 14:04:56 +0400

    + NewsVC Frame Convenience

--- Puntr/NewsViewController.m
+++ Puntr/NewsViewController.m
@@ -28,16 +28,9 @@ static const CGFloat TNItemSpacing = 12.0f;
     [self addBalanceButton];
     self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
     
-    CGRect applicationFrame = [[UIScreen mainScreen] applicationFrame];
-    CGRect viewControllerFrame = CGRectMake(0.0f,
-                                            0.0f,
-                                            CGRectGetWidth(applicationFrame),
-                                            CGRectGetHeight(applicationFrame) - CGRectGetHeight(self.navigationController.navigationBar.bounds) - CGRectGetHeight(self.tabBarController.tabBar.bounds)
-                                            );
-    
     self.collectionManager = [CollectionManager managerWithType:CollectionTypeNews modifierObject:nil];
     UICollectionView *collectionView = self.collectionManager.collectionView;
-    collectionView.frame = viewControllerFrame;
+    collectionView.frame = self.frame;
     [self.view addSubview:collectionView];
 }
 
