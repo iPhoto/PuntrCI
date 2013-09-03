0a950ae4c3e9ea90feb3f52c327224a67a6e192e
Eugene Tulushev <eugene@tulushev.com>
2013-09-03 16:31:00 +0400

    + UIVC+Puntr Button Filter

--- Puntr/UIViewController+Puntr.m
+++ Puntr/UIViewController+Puntr.m
@@ -6,10 +6,11 @@
 //  Copyright (c) 2013 2Nova Interactive. All rights reserved.
 //
 
-#import "UIViewController+Puntr.h"
-#import "ObjectManager.h"
-#import "NotificationManager.h"
+#import "FilterViewController.h"
 #import "MoneyModel.h"
+#import "NotificationManager.h"
+#import "ObjectManager.h"
+#import "UIViewController+Puntr.h"
 
 @implementation UIViewController (Puntr)
 
@@ -17,17 +18,31 @@
 {
     UIButton *buttonBalance = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 64, 30)];
     [buttonBalance setBackgroundImage:[[UIImage imageNamed:@"ButtonBar"] resizableImageWithCapInsets:UIEdgeInsetsMake(0.0f, 7.0f, 0.0f, 7.0f)] forState:UIControlStateNormal];
-    //[self.buttonSubscribe setTitle:@"Подписаться" forState:UIControlStateNormal];
     [buttonBalance.titleLabel setFont:[UIFont fontWithName:@"Arial-BoldMT" size:15.0f]];
     buttonBalance.titleLabel.shadowColor = [UIColor blackColor];
     buttonBalance.titleLabel.shadowOffset = CGSizeMake(0.0f, -1.5f);
     [buttonBalance.titleLabel setTextColor:[UIColor whiteColor]];
     [buttonBalance.titleLabel setTextAlignment:NSTextAlignmentRight];
-    //[buttonBalance.titleLabel setBackgroundColor:[UIColor redColor]];
     [buttonBalance setImage:[UIImage imageNamed:@"IconMoney"] forState:UIControlStateNormal];
-    //CGRectGetWidth(buttonBalance.frame)
     [buttonBalance setImageEdgeInsets:UIEdgeInsetsMake(0.0, CGRectGetWidth(buttonBalance.frame) - 21.0, 0.0, 0.0)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:buttonBalance];
+}
+
+- (void)addFilterButton
+{
+    UIButton *buttonFilter = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    [buttonFilter setBackgroundImage:[[UIImage imageNamed:@"ButtonBar"] resizableImageWithCapInsets:UIEdgeInsetsMake(0.0f, 7.0f, 0.0f, 7.0f)] forState:UIControlStateNormal];
+    [buttonFilter setImage:[UIImage imageNamed:@"IconFilter"] forState:UIControlStateNormal];
+    [buttonFilter setImageEdgeInsets:UIEdgeInsetsMake(0, 5, 0, 4)];
+    [buttonFilter addTarget:self action:@selector(touchedButtonFilter) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *settingsItem = [[UIBarButtonItem alloc] initWithCustomView:buttonFilter];
+    self.navigationItem.leftBarButtonItem = settingsItem;
+}
+
+- (void)touchedButtonFilter
+{
+    FilterViewController *filterViewController = [FilterViewController new];
+    [self.navigationController pushViewController:filterViewController animated:YES];
 }
 
 - (void)updateBalance
@@ -53,7 +68,6 @@
             }
             [(UIButton *)self.navigationItem.rightBarButtonItem.customView setTitle:stringBallance
                                                                            forState:UIControlStateNormal];
-            //CGRectGetWidth([(UIButton *)self.navigationItem.rightBarButtonItem.customView titleLabel].frame);
             CGSize maxLabelSize = [@"0000" sizeWithFont:[(UIButton *)self.navigationItem.rightBarButtonItem.customView titleLabel].font];
             [(UIButton *)self.navigationItem.rightBarButtonItem.customView setTitleEdgeInsets:UIEdgeInsetsMake(0.0, MAX(-8.0f, -8.0f + maxLabelSize.width - CGRectGetWidth([(UIButton *)self.navigationItem.rightBarButtonItem.customView titleLabel].frame)), 0.0, 23.0)];
         }
