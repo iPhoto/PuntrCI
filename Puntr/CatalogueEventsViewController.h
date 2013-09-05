55c416407a085bd429d0fe5173b7cf0dd1dbc69a
Eugene Tulushev <eugene@tulushev.com>
2013-09-05 18:36:43 +0400

    ~ CatalogueVC to CatalogueEventsVC

--- Puntr/CatalogueEventsViewController.h
+++ Puntr/CatalogueEventsViewController.h
@@ -1,5 +1,5 @@
 //
-//  CatalogueViewController.h
+//  CatalogueEventsViewController.h
 //  Puntr
 //
 //  Created by Eugene Tulushev on 14.06.13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CatalogueViewController : UIViewController
+@interface CatalogueEventsViewController : UIViewController
 
 @end
