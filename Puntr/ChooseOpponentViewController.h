4b8e6dd91349f2435d93c5c216e171b7d085c249
artik818 <artik818@gmail.com>
2013-09-08 23:45:55 +0300

    + choose opponent screen

---
+++ Puntr/ChooseOpponentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseOpponentViewController.h
+//  Puntr
+//
+//  Created by Artem on 9/6/13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseOpponentViewController : UIViewController
+
+@end
