d4af7017cb51c1fcf0ee4cc5add9d3413717c22c
Eugene Tulushev <eugene@tulushev.com>
2013-09-12 20:17:22 +0400

    ~ MyStakesVC CollectionManager

--- Puntr/MyStakesViewController.m
+++ Puntr/MyStakesViewController.m
@@ -6,15 +6,11 @@
 //  Copyright (c) 2013 2Nova Interactive. All rights reserved.
 //
 
+#import "CollectionManager.h"
 #import "MyStakesViewController.h"
 #import "UIViewController+Puntr.h"
-#import "CollectionManager.h"
-
-static const CGFloat TNItemSpacing = 12.0f;
 
 @interface MyStakesViewController ()
-
-@property (nonatomic, strong) UISegmentedControl *segmentedControl;
 
 @property (nonatomic, strong) CollectionManager *collectionManager;
 
@@ -30,32 +26,9 @@ static const CGFloat TNItemSpacing = 12.0f;
     [self addBalanceButton];
     self.view.backgroundColor = [UIColor colorWithWhite:0.302 alpha:1.000];
     
-    CGRect applicationFrame = [[UIScreen mainScreen] applicationFrame];
-    CGRect viewControllerFrame = CGRectMake(0.0f,
-                                            0.0f,
-                                            CGRectGetWidth(applicationFrame),
-                                            CGRectGetHeight(applicationFrame) - CGRectGetHeight(self.navigationController.navigationBar.bounds) - CGRectGetHeight(self.tabBarController.tabBar.bounds)
-                                            );
-    
-    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Ставки", @"Пари"]];
-    self.segmentedControl.frame = CGRectMake(15.0f, TNItemSpacing, 290.0f, 31.0f);
-    self.segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
-    self.segmentedControl.tintColor = [UIColor blackColor];
-    [self.view addSubview:self.segmentedControl];
-    dispatch_async(dispatch_get_main_queue(), ^
-        {
-            self.segmentedControl.selectedSegmentIndex = 1;
-        }
-    );
-    
     self.collectionManager = [CollectionManager managerWithType:CollectionTypeMyStakes modifierObjects:nil];
     UICollectionView *collectionView = self.collectionManager.collectionView;
-    collectionView.frame = CGRectMake(
-                                      0.0f,
-                                      CGRectGetMaxY(self.segmentedControl.frame) + TNItemSpacing,
-                                      CGRectGetWidth(viewControllerFrame),
-                                      CGRectGetHeight(viewControllerFrame) - (CGRectGetMaxY(self.segmentedControl.frame) + TNItemSpacing)
-                                     );
+    collectionView.frame = self.frame;
     [self.view addSubview:collectionView];
 }
 
