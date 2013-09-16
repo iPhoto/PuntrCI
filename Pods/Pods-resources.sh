80c0d369dfb89e543b45446772627d214d7fbcee
Eugene Tulushev <eugene@tulushev.com>
2013-09-16 19:08:44 +0400

    + SVProgressHUD

--- Pods/Pods-resources.sh
+++ Pods/Pods-resources.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
-RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy.txt
-touch "$RESOURCES_TO_COPY"
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
+> "$RESOURCES_TO_COPY"
 
 install_resource()
 {
@@ -15,12 +16,22 @@ install_resource()
       ibtool --reference-external-strings-file --errors --warnings --notices --output-format human-readable-text --compile "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename \"$1\" .xib`.nib" "${PODS_ROOT}/$1" --sdk "${SDKROOT}"
       ;;
     *.framework)
-      echo "rsync -rp ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
-      rsync -rp "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      mkdir -p "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      echo "cp -fpR ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      cp -fpR "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      ;;
+    *.xcdatamodel)
+      echo "xcrun momc \"${PODS_ROOT}/$1\" \"${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1"`.mom\""
+      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodel`.mom"
       ;;
     *.xcdatamodeld)
-      echo "xcrun momc ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
-      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
+      echo "xcrun momc \"${PODS_ROOT}/$1\" \"${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd\""
+      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd"
+      ;;
+    /*)
+      echo "$1"
+      echo "$1" >> "$RESOURCES_TO_COPY"
       ;;
     *)
       echo "${PODS_ROOT}/$1"
@@ -28,121 +39,122 @@ install_resource()
       ;;
   esac
 }
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'FormatterKit/Localizations/da.lproj'
-install_resource 'FormatterKit/Localizations/de.lproj'
-install_resource 'FormatterKit/Localizations/el.lproj'
-install_resource 'FormatterKit/Localizations/en.lproj'
-install_resource 'FormatterKit/Localizations/es.lproj'
-install_resource 'FormatterKit/Localizations/fr.lproj'
-install_resource 'FormatterKit/Localizations/id.lproj'
-install_resource 'FormatterKit/Localizations/it.lproj'
-install_resource 'FormatterKit/Localizations/nb.lproj'
-install_resource 'FormatterKit/Localizations/nl.lproj'
-install_resource 'FormatterKit/Localizations/nn.lproj'
-install_resource 'FormatterKit/Localizations/pt_BR.lproj'
-install_resource 'FormatterKit/Localizations/ru.lproj'
-install_resource 'FormatterKit/Localizations/sv.lproj'
-install_resource 'TSMessages/Resources/design.json'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundError.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundError@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundErrorIcon.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundErrorIcon@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundMessage.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundMessage@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundSuccess.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundSuccess@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundSuccessIcon.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundSuccessIcon@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundWarning.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundWarning@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundWarningIcon.png'
-install_resource 'TSMessages/Resources/Images/NotificationBackgroundWarningIcon@2x.png'
-install_resource 'TSMessages/Resources/Images/NotificationButtonBackground.png'
-install_resource 'TSMessages/Resources/Images/NotificationButtonBackground@2x.png'
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "FormatterKit/Localizations/da.lproj"
+install_resource "FormatterKit/Localizations/de.lproj"
+install_resource "FormatterKit/Localizations/el.lproj"
+install_resource "FormatterKit/Localizations/en.lproj"
+install_resource "FormatterKit/Localizations/es.lproj"
+install_resource "FormatterKit/Localizations/fr.lproj"
+install_resource "FormatterKit/Localizations/id.lproj"
+install_resource "FormatterKit/Localizations/it.lproj"
+install_resource "FormatterKit/Localizations/nb.lproj"
+install_resource "FormatterKit/Localizations/nl.lproj"
+install_resource "FormatterKit/Localizations/nn.lproj"
+install_resource "FormatterKit/Localizations/pt_BR.lproj"
+install_resource "FormatterKit/Localizations/ru.lproj"
+install_resource "FormatterKit/Localizations/sv.lproj"
+install_resource "SVProgressHUD/SVProgressHUD/SVProgressHUD.bundle"
+install_resource "TSMessages/Resources/design.json"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundError.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundError@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundErrorIcon.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundErrorIcon@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundMessage.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundMessage@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundSuccess.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundSuccess@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundSuccessIcon.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundSuccessIcon@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundWarning.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundWarning@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundWarningIcon.png"
+install_resource "TSMessages/Resources/Images/NotificationBackgroundWarningIcon@2x.png"
+install_resource "TSMessages/Resources/Images/NotificationButtonBackground.png"
+install_resource "TSMessages/Resources/Images/NotificationButtonBackground@2x.png"
 
 rsync -avr --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
-rm "$RESOURCES_TO_COPY"
+rm -f "$RESOURCES_TO_COPY"
