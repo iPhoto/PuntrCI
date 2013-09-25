3fe15934b942948229c774f52d80a26284704ab5
Eugene Tulushev <eugene@tulushev.com>
2013-09-25 17:08:09 +0400

    + SearchModel

---
+++ Puntr/SearchModel.h
@@ -0,0 +1,15 @@
+//
+//  SearchModel.h
+//  Puntr
+//
+//  Created by Eugene Tulushev on 23.09.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchModel : NSObject
+
+@property (nonatomic, strong) NSString *query;
+
+@end
---
+++ Puntr/SearchModel.m
@@ -0,0 +1,13 @@
+//
+//  SearchModel.m
+//  Puntr
+//
+//  Created by Eugene Tulushev on 23.09.13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import "SearchModel.h"
+
+@implementation SearchModel
+
+@end
