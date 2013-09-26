7f68194d264e41d2fbd886230a7573c1a93cd179
Alexander Lebedev <alexander.lebedev@2nova.ru>
2013-09-26 17:10:03 +0400

    + friends from socials controller

---
+++ Puntr/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Puntr
+//
+//  Created by Alexander Lebedev on 9/26/13.
+//  Copyright (c) 2013 2Nova Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
